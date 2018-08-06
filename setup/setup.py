df5aa04d8a725d8d5ccb65094d4386ec807308ec
Aaron Simmons <paleozogt@gmail.com>
2018-08-06 13:16:07 -0600

    building platform-specific wheels with bundled libclang

--- setup/setup.py
+++ setup/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, dist
 from os import path
 import sys
 
@@ -9,14 +9,25 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+# default for set_library_path
+with open("clang/cindex.py", "a") as initfile:
+    initfile.write(u'''
+import os
+import cindex
+cindex.Config.set_library_path(os.path.dirname(os.path.abspath(__file__)))
+''')
+
+# get the version
+version= open('version.txt').read()
 
 setup(
     name="clang",
     author="Ethan Smith",
     author_email="ethan@ethanhs.me",
-    version="6.0.0.2",
+    version=version,
     url="https://github.com/ethanhs/clang",
     packages=["clang"],
+    include_package_data=True,
     long_description=long_description,
     long_description_content_type='text/markdown'
 )
